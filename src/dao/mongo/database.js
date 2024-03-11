53bfcfa6dd4fc732717d0861376f265044d99843
davidisnardi <davidisnardi38@gmail.com>
2024-03-10 22:13:27 -0300

    Reestructura de nuestro servidor

---
+++ src/dao/mongo/database.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+  console.log('Conexión exitosa a MongoDB');
+});
+
+export default conectarBD;
